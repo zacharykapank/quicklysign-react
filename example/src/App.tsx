eda405d3abe763b561fadfc926203b1ad8fb5214
Zachary <zachary.kapank@sovtech.com>
2020-12-04 19:01:14 +0200

    refactor: remove unused code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,26 +4,8 @@ import QuicklySign from 'quicklysign-react'
 import 'quicklysign-react/dist/index.css'
 
 const App = () => {
-  // const config = {
-  //   name_visible: false,
-  //   signatory_details_visible: false,
-  //   settings_visible: false,
-  //   user_defined_attributes_visible: false,
-  //   tabs_visible: true,
-  //   upload_new_document_enabled: false
-  // }
-
-  // const toggle_upload_enabled = () => {
-  //   config.upload_new_document_enabled = !config.upload_new_document_enabled
-  //   return config
-  // }
-
   return (
-    <QuicklySign
-      client_id='ahFzfnRoZW1hc3NpdmUtbGl2ZXIYCxILQXBwbGljYXRpb24YgIDA_rXk2AgMogEHc2FuZGJveA'
-      url='https://sandbox.quicklysign.com/view?token=CsHU5ep7V7YQHBo2udmFs8pNidC3pG7dmi9clKLGIob'
-      post_sign_url=''
-    >
+    <QuicklySign client_id='' url='' post_sign_url=''>
       {({ update_ui_configuration }) => (
         <div>
           <button
@@ -38,7 +20,7 @@ const App = () => {
               })
             }
           >
-            some button
+            some button to update ui
           </button>
         </div>
       )}
